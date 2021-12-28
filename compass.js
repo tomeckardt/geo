8b864be13be24bb225ceae15f13a08d4b7501e53
Tom Eckardt <Ta10top-twcubatwc>
2021-12-28 16:22:52 +0100

    Erster Test

---
+++ compass.js
@@ -0,0 +1,14 @@
+
+const isIOS =
+      navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+      navigator.userAgent.match(/AppleWebKit/)
+
+let paragraph = document.getElementById("alpha")
+
+if (isIOS) {
+    paragraph.innerHTML = "Apple Fanboy"
+} else {
+    window.addEventListener("deviceorientationabsolute", function(event) {
+        paragraph.innerHTML = event.alpha.toString()
+    }, true)
+}
