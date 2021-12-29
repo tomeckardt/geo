966bc91f0c0763cb033af76fa41c671769f3bc52
Tom Eckardt <Ta10top-twcubatwc>
2021-12-29 12:56:25 +0100

    Test7

--- compass.js
+++ compass.js
@@ -6,12 +6,15 @@ const isAndroidFirefox = navigator.userAgent.match(/Firefox/) &&
 
 
 let paragraph = document.querySelector("#mag_alpha")
+let gamma = document.querySelector("#mag_gamma")
 console.log(paragraph)
 document.querySelector("#mag_beta").innerHTML = navigator.userAgent.toString()
 
 if (window.DeviceOrientationEvent) {
     if (isIOS) {
+        gamma.innerHTML = "Apple Fanboy"
         DeviceOrientationEvent.requestPermission().then(value => {
+            gamma.innerHTML = value
             if (value === "granted") {
                 window.addEventListener("deviceorientation", function(event) {
                     update(event.webkitCompassHeading)
