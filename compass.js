0336387d9a66228b55a16112acdf4339db70d121
Tom Eckardt <Ta10top-twcubatwc>
2021-12-29 12:18:04 +0100

    Test5

--- compass.js
+++ compass.js
@@ -1,26 +1,32 @@
 console.log("Test")
-const isIOS =
-      navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
-      navigator.userAgent.match(/AppleWebKit/)
+const isIOS = navigator.userAgent.match(/AppleWebKit/)
+const isAndroidFirefox = navigator.userAgent.match(/Firefox/) &&
+    navigator.userAgent.match(/Android/)
+
 
 let paragraph = document.querySelector("#mag_alpha")
 console.log(paragraph)
 document.querySelector("#mag_beta").innerHTML = navigator.userAgent.toString()
 
-if (DeviceOrientationEvent) {
+if (window.DeviceOrientationEvent) {
     if (isIOS) {
-        DeviceOrientationEvent.requestPermission().then((value) => {
+        DeviceOrientationEvent.requestPermission().then(value => {
             if (value === "granted") {
                 window.addEventListener("deviceorientation", function(event) {
                     update(event.webkitCompassHeading)
                 }, true)
             }
         })
+    } else if (isAndroidFirefox) {
+        //TODO
+        update("Firefox wird nicht unterstützt")
     } else {
         window.addEventListener("deviceorientationabsolute", function (event) {
             update(event.alpha)
         }, true)
     }
+} else {
+    update("Feature wird nicht unterstützt")
 }
 
 function update(orientation) {
