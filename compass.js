f4b9b5c5051c35cc8b4b6d6c68495e293063668a
Tom Eckardt <Ta10top-twcubatwc>
2021-12-31 17:18:22 +0100

    Rechenfehler korrigiert - Teil 3

--- compass.js
+++ compass.js
@@ -63,14 +63,16 @@ async function init() {
 }
 
 const compass = document.querySelector('#compass')
+let lastOrientation = 0
 function update(orientation) {
     if (locationKnown && orientationKnown) {
         let city = nearestBinarySearch(360 - orientation)
         document.querySelector('#cityname').innerHTML = city.asciiname
-        compass.style.mozTransform    = 'rotate(-'+orientation+'deg)';
-        compass.style.msTransform     = 'rotate(-'+orientation+'deg)';
-        compass.style.oTransform      = 'rotate(-'+orientation+'deg)';
-        compass.style.transform       = 'rotate(-'+orientation+'deg)';
+        let deg = orientation - lastOrientation
+        compass.style.mozTransform    = 'rotate('+deg+'deg)';
+        compass.style.msTransform     = 'rotate('+deg+'deg)';
+        compass.style.oTransform      = 'rotate('+deg+'deg)';
+        compass.style.transform       = 'rotate('+deg+'deg)';
     } else {
         //TODO
     }
