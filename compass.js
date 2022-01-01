ce7f00eea55259f353800b9e48fc2da808b965a6
Tom Eckardt <Ta10top-twcubatwc>
2022-01-01 17:24:07 +0100

    Die Distanz der Städte wird angezeigt

--- compass.js
+++ compass.js
@@ -66,11 +66,14 @@ function initGame() {
 }
 
 const compass = document.querySelector('#compass')
+const cityname = document.querySelector("#cityname")
+const citydistance = document.querySelector("#citydistance")
 let lastOrientation = 0
 function update(orientation) {
     if (locationKnown && orientationKnown) {
         let city = nearestBinarySearch(orientation)
-        document.querySelector('#cityname').innerHTML = city.asciiname
+        cityname.innerHTML = city.asciiname
+        citydistance.innerHTML = city.relDistance
         let deg = lastOrientation - orientation
         compass.style.mozTransform    = 'rotate('+deg+'deg)';
         compass.style.msTransform     = 'rotate('+deg+'deg)';
