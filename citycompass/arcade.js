5b0291d7b8e0efd310ef85734b0c3f7ed5582b15
Tom Eckardt <Ta10top-twcubatwc>
2022-01-14 13:09:05 +0100

    Kleines Experiment - Teil 2

---
+++ citycompass/arcade.js
@@ -0,0 +1,109 @@
+import LatLon from 'https://cdn.jsdelivr.net/npm/geodesy@2/latlon-spherical.min.js'
+
+/*
+Städte laden
+*/
+let data
+loadData().then(() => console.log("Daten geladen"))
+async function loadData() {
+    const promise = await fetch("data.json")
+    data = await promise.json()
+    initGeoLocation()
+}
+
+/*
+Button initialisieren
+*/
+document.querySelector("#permission_btn").onclick = init;
+
+let playerPosition
+function initGeoLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            const lat = position.coords.latitude
+            const lon = position.coords.longitude
+            playerPosition = LatLon.parse(lat, lon)
+            data.forEach(e => {
+                const p = LatLon.parse(e.latitude, e.longitude)
+                e.relAngle = playerPosition.initialBearingTo(p)
+                e.relDistance = Math.round(playerPosition.distanceTo(p) / 1000)
+            })
+            data.sort((e1, e2) => e1.relAngle - e2.relAngle)
+        },
+            () => {
+            document.querySelector(".main_content").innerHTML =
+                `<h2 >Kein Zugriff auf deine Standortdaten</h2>
+                 <p>Ohne den Standort funktioniert die Seite nicht</p>
+                `
+        })
+    }
+}
+
+async function init() {
+    function noCompass() {
+        document.querySelector(".main_content").innerHTML =
+            `<h2>Kein Zugriff auf deinen Kompass</h2>
+             <p>Die Daten deines Kompass werden benötigt. Bist du auf einem mobilen Endgerät, das einen Kompass unterstützt?</p>
+            `
+    }
+    if (DeviceMotionEvent.requestPermission !== undefined) { //Safari
+        await DeviceMotionEvent.requestPermission().then(permissionState => {
+            if (permissionState === 'granted') {
+                initGame()
+                window.addEventListener('deviceorientation', event => {
+                    update(event.webkitCompassHeading)
+                })
+            } else {
+                noCompass()
+            }
+        })
+    } else {//Chrome
+        let hasStarted = false;
+        window.addEventListener("deviceorientationabsolute", function (event) {
+            if (event.alpha != null) {
+                if (!hasStarted) {
+                    initGame()
+                    hasStarted = true
+                }
+                update(360 - event.alpha)
+            }
+        }, true)
+        noCompass()
+    }
+}
+
+function initGame() {
+    let scoreText = document.querySelector('#score')
+    let button = document.querySelector('#permission_btn')
+    let cityText = document.querySelector('#cityname')
+    let city = getRandomCity()
+    cityText.innerHTML = city.asciiname
+    button.innerHTML = "Diese Richtung"
+    button.onclick = function () {
+        scoreText.innerHTML = getScore(city)
+        city = getRandomCity()
+        cityText.innerHTML = city.asciiname
+    }
+}
+
+const compass = document.querySelector('#compass')
+let playerOrientation = 0
+function update(orientation) {
+    playerOrientation = orientation
+    //Kompass
+    let deg = - orientation
+    compass.style.mozTransform    = 'rotate('+deg+'deg)';
+    compass.style.msTransform     = 'rotate('+deg+'deg)';
+    compass.style.oTransform      = 'rotate('+deg+'deg)';
+    compass.style.transform       = 'rotate('+deg+'deg)';
+}
+
+function getRandomCity() {
+    return data[Math.floor(Math.random() * (data.length + 1))]
+}
+
+function getScore(city) {
+    const factor = 1000
+    let distance = LatLon.destinationPoint(city.relDistance, playerOrientation).distanceTo(playerPosition)
+    return 2 * factor - Math.round(factor * distance / city.relDistance)
+}
